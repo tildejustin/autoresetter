1b6fb09880ebecd380133252262d8f3beba56e35
tildejustin <spacepepper3.14@gmail.com>
2023-02-26 22:25:14 -0500

    check struct dist because mc doesn't know how ig

--- src/main/java/xyz/tildejustin/autoresetter/mixin/LevelLoadingScreenMixin.java
+++ src/main/java/xyz/tildejustin/autoresetter/mixin/LevelLoadingScreenMixin.java
@@ -5,6 +5,7 @@ import me.voidxwalker.worldpreview.WorldPreview;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.LevelLoadingScreen;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,9 +22,8 @@ public abstract class LevelLoadingScreenMixin {
     private void render(CallbackInfo ci) {
         if (WorldPreview.world != null && WorldPreview.player != null && WorldPreview.clientWord != null && !Autoresetter.checkedReset) {
             boolean reset = true;
-            BlockPos spawnPos = WorldPreview.player.getBlockPos();
             if (Autoresetter.biomes.length != 0) {
-                Biome spawnBiome = WorldPreview.player.getEntityWorld().getBiome(spawnPos);
+                Biome spawnBiome = WorldPreview.player.getEntityWorld().getBiome(WorldPreview.spawnPos);
                 for (Biome biome : Autoresetter.biomes) {
                     if (spawnBiome.getClass().equals(biome.getClass())) {
                         reset = false;
@@ -33,9 +33,15 @@ public abstract class LevelLoadingScreenMixin {
             }
             if (Autoresetter.structures.length > 0 && reset) {
                 for (Structure structure : Autoresetter.structures) {
-                    if (WorldPreview.world.getServer().getOverworld().locateStructure(structure.getStructure(), spawnPos, structure.radius, false) != null) {
-                        reset = false;
-                        break;
+                    BlockPos structureLocation = WorldPreview.world.getServer().getOverworld().locateStructure(structure.getStructure(), WorldPreview.spawnPos, structure.radius, false);
+                    System.out.println("structure: " + structureLocation);
+                    if (structureLocation != null) {
+                        System.out.println(structure + ": " + structureLocation.getX() + " " + structureLocation.getZ());
+                        System.out.println("current pos:" + WorldPreview.spawnPos.getX() + " " + WorldPreview.spawnPos.getZ());
+                        if (Math.abs((WorldPreview.spawnPos.getX() << 4) - (structureLocation.getX() << 4)) <= structure.radius || Math.abs((WorldPreview.spawnPos.getZ() << 4) - (structureLocation.getZ() << 4)) <= structure.radius) {
+                            reset = false;
+                            break;
+                        }
                     }
                 }
             }
